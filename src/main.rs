46745183df2ea1ad6aa444bfd169b29c850d51e6
David Kamayor <davidkamayor@gmail.com>
2024-07-04 16:49:20 -0400

    add usage warning if rom not supplied

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.leng() != 2 {
+        println!("Usage: cargo run path/to/game");
+        return;
+    }
 }
