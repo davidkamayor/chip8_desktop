8554b554e1419165261b1cfae8df19c5db6335c7
David Kamayor <davidkamayor@gmail.com>
2024-07-04 17:26:00 -0400

    implement SDL setup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use chip8_core::*;
 use std::env;
 
+const SCALE: u32 = 15;
+const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+const WINDOW_HEIGHT: U32 = (SCREEN_HEIGHT as u32) * SCALE;
 fn main() {
+    // check if rom is supplied
     let args: Vec<_> = env::args().collect();
     if args.leng() != 2 {
         println!("Usage: cargo run path/to/game");
         return;
     }
+
+    // Setup SDL
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Chip-8 Emulator", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.clear();
+    canvas.present();
 }
